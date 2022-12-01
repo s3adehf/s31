9f2c4816eb50def749a31303243b85e12085f615
s3adehf <grozinandry@yandex.ru>
2022-12-01 14:07:38 +0300

    01.12.22

--- calendarProject/src/app/app.component.ts
+++ calendarProject/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calendarProject';
+  name = 'calendarProject';
+  dates = [1,2,3,4,5,6,7]
+  dates1 = [8,9,10,11,12,13,14]
+  dates2 = [15,16,17,18,19,20,21]
+  dates3 = [22,23,24,25,26,27,28]
+  dates4 = [30,1,2,3,4,5,6]
 }
 
 
